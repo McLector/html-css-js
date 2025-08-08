1f9ece9ad99822bb7022f790c018b4830ce3796d
Myre Lector Andre Morada <myre_lector_andre_morada@dlsl.edu.ph>
2025-08-08 22:42:18 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Thank you for your message!");
+  return false;
+}
+
+function changeBackground() {
+  document.getElementById('portfolio').style.backgroundColor = '#e0f7fa';
+}
